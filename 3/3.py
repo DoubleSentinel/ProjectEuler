4e136138135a36fb5f5aa049514abb03c13f53df
Karim Luy <karim.luy@epfl.ch>
2017-12-01 15:27:48 +0100

    moved files in sub folders; added two new members

--- 3/3.py
+++ 3/3.py
@@ -11,15 +11,14 @@ def biggestprime(n):
 	largest = 0
 	max = n
 	#this condition is used because using the square root of a number as a relatively good guess 
-	#only becomes useful when the natural number becomes really large (5 digits is a good starting point
-	if len(str(n)) >= 5:
+	#only becomes useful when the natural number becomes really large (5 digits is a good starting point)
+	if n > 9999:
 		max = math.ceil(math.pow(n,0.5))
 	#the reason we're looking at the square root of the value is described in the Quadratic Sieve algorithm
 	#also, general modular mathematics and Fermat Theorem
 	for i in range(2,max):
-		if not n%i:
-			if is_prime(i):
-				largest = i
+		if not n%i and is_prime(i):
+			largest = i
 	return largest
 	
 if __name__ == '__main__':
