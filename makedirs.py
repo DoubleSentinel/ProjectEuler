d6006cb06ba43a6fe748b0fbd2ec02caaba2e2fe
Karim Luy <karim.luy@epfl.ch>
2017-12-01 18:08:27 +0100

    got makedirs to work with both argument types

--- makedirs.py
+++ makedirs.py
@@ -6,13 +6,10 @@ if __name__ == '__main__':
 	try:
 		if sys.argv[1] == "clean":
 			for root, dirs, files in os.walk('./exercises/'):
-				if len(files) <= 1:
+				if len(files) == 1:
 					for name in files:
-						print('file: ' + os.path.join(root, name))
-						#os.remove(os.path.join(root, name))
-					for name in dirs:
-						print('dir: ' + os.path.join(root, name))
-						#os.rmdir(os.path.join(root, name))
+						os.remove(os.path.join(root, name))
+						os.rmdir(root)
 		elif sys.argv[1] and sys.argv[2]:
 			url = "https://projecteuler.net/problem="
 			for i in range(int(sys.argv[1]),int(sys.argv[2])+1):
@@ -20,9 +17,9 @@ if __name__ == '__main__':
 					page = response.read()
 					soup = BeautifulSoup(page, 'html.parser')
 					problem_content = soup.findAll("div",{'class' : 'problem_content'})
-					if not os.path.exists('./'+str(i)):
-						os.makedirs('./'+str(i))
-					htmlfile = open('./'+str(i)+'/'+str(i)+'.html', 'w')
+					if not os.path.exists('./exercises/'+str(i)):
+						os.makedirs('./exercises/'+str(i))
+					htmlfile = open('./exercises/'+str(i)+'/'+str(i)+'.html', 'w')
 					htmlfile.write(str(problem_content).split('[')[1].split(']')[0])
 					htmlfile.close()
 		
