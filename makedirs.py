052c1474281b3b141674a6bc1df276cd651e923e
roflolilolmao <kelstar91@hotmail.com>
2017-12-07 21:28:05 +0100

    Renamed folders to make them compatible with haskell

--- makedirs.py
+++ makedirs.py
@@ -1,8 +1,11 @@
 import urllib.request
-import os, sys, shutil
+import os
+import sys
+import shutil
 from bs4 import BeautifulSoup
 
-if __name__ == '__main__':
+
+def main():
 	try:
 		if len(sys.argv[1:]) == 1 and sys.argv[1] == "clean":
 			for root, dirs, files in os.walk('./exercises/'):
@@ -10,16 +13,24 @@ if __name__ == '__main__':
 					for name in files:
 						os.remove(os.path.join(root, name))
 						os.rmdir(root)
-		elif len(sys.argv[1:]) > 1:
-			url = "https://projecteuler.net/problem="
-			for i in range(int(sys.argv[1]),int(sys.argv[2])+1):
-				with urllib.request.urlopen(url+str(i)) as response:
+                        
+		elif len(sys.argv[1:]) == 2:
+			url = "https://projecteuler.net/problem={}"
+            
+            low = int(sys.argv[1])
+            high = int(sys.argv[2])
+            
+			for i in range(low, high + 1):
+				with urllib.request.urlopen(url.format(i)) as response:
 					page = response.read()
 					soup = BeautifulSoup(page, 'html.parser')
-					problem_content = soup.findAll("div",{'class' : 'problem_content'})
-					if not os.path.exists('./exercises/'+str(i)):
-						os.makedirs('./exercises/'+str(i))
-					htmlfile = open('./exercises/'+str(i)+'/'+str(i)+'.html', 'w')
+					problem_content = soup.findAll("div", {'class': 'problem_content'})
+					
+                    folder = './exercises/{}'.format(i)
+                    if not os.path.exists(folder):
+						os.makedirs(folder)
+					
+                    htmlfile = open('{}/{}.html'.format(folder, i), 'w')
 					htmlfile.write(str(problem_content).split('[')[1].split(']')[0])
 					htmlfile.close()
 		
@@ -27,10 +38,13 @@ if __name__ == '__main__':
 			print("Makedirs will create all the subdirectories for Project Euler problems N to M")
 			print("To perform this use the arguments python ./makedirs N M")
 			print("To clean up unchanged directories use the argument python ./makedirs clean")
-	except urllib.error.HTTPError as e:
-		if e.code == 404:
-			exit()
+	except urllib.error.HTTPError:
+		print('rekt by http')
 	except UnicodeEncodeError:
-		pass
-	except IndexError:
-		pass
+		print('rekt by unicode')
+	except:
+		print('rekt by existence')
+        
+        
+if __name__ == '__main__':
+    main()
